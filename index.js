1ce52858b2d5d776ced52e8129b3a7b4ab00a96b
Jesus Manuel Bobadilla <aldinhobobadilla@gmail.com>
2026-04-01 12:23:19 -0600

    feat: Se agrega  el cron job para ejecucion de worker cada minuto

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+import cron from 'node-cron';
 import getInfoDevices from './src/api/wialon.js';
 import { destructWialon } from './src/utils/utils.js';
 
 const app = async () => {
   try {
-    
     const data = await getInfoDevices();
     const devices = destructWialon(data);
+
     console.table(devices);
-    
   } catch (error) {
-    console.error('❌ Error:', error);
+    console.error('❌ Error:', error.message);
   }
 };
 
 app();
-
-// setInterval(app, 60000);
+// Ejecutar cada minuto
+cron.schedule('* * * * *', async () => {
+  await app();
+});
